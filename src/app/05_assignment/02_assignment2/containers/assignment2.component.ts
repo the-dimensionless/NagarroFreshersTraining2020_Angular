568f0d186fce80d9e2503ab7d7e05a9d6ccf1bca
Sumit Singh <sumit.singh@nagarro.com>
2020-04-21 16:47:40 +0530

    Completed Assignment2 Successfully

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from 'src/app/05_assignment/assignment.component';
 
 @Component({
   selector: 'app-assignment2',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment2.component.css']
 })
 export class Assignment2Component {
+  @Input() todos: Todo[];
 
+  // Testing 
+  /*  ngOnChanges() {
+     console.log(this.todos);
+   } */
 }
